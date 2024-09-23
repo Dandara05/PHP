89db73f1e94ce7076a39d35fd0aee60114faa0b7
Dandara Ramos <dandara.versutti@etec.sp.gov.br>
2024-09-23 20:41:10 -0300

    PHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <form action="script.php" method="post">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome">
+    <label for="nota1">Nota1</label>
+    <input type="number" name="nota1" id="Nota1">
+    <label for="nota2">Nota2</label>
+    <input type="number" name="nota2" id="Nota2">
+    <label for="nota3">Nota3</label>
+    <input type="number" name="nota3" id="Nota3">
+    <label for="nota4">Nota4</label>
+    <input type="number" name="nota4" id="Nota4">
+    
+    <button type="submit">Enviar</button>
+
+    </form>
+
+</body>
+</html>
